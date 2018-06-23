c9a68384635676cd3d4ab20e32e3e7ea2f50f58d
Subrat <subratsahu93@yahoo.com>
2018-06-24 01:58:55 +0530

    no more maintenance

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use((req, res, next) => {
 });
 
 app.use((req,res,next)=>{
-    res.render('maintenance.hbs');
+    // res.render('maintenance.hbs');
+    next();
 });
 
 app.use(express.static(__dirname + '/public'));
